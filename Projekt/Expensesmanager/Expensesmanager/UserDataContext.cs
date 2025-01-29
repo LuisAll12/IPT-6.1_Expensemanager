a05fe207aca688a35d3d29a81804784b4436f79c
Luis Allamand <Luis_Allamand@sluz.ch>
2025-01-29 20:28:00 +0100

    Changes in UI/UX for the Home Dashboard component

---
+++ Projekt/Expensesmanager/Expensesmanager/UserDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expensesmanager
+{
+    public class UserDataContext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    }
+}
