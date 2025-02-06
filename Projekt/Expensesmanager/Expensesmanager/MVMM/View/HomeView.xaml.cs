7ee6720c7a21434bb821bab811389a2a6934da6c
Luis Allamand <Luis_Allamand@sluz.ch>
2025-02-06 19:14:35 +0100

    Added better UI in Home view and good a Loader

--- Projekt/Expensesmanager/Expensesmanager/MVMM/View/HomeView.xaml.cs
+++ Projekt/Expensesmanager/Expensesmanager/MVMM/View/HomeView.xaml.cs
@@ -29,8 +29,12 @@ namespace Expensesmanager.MVMM.View
         {
             InitializeComponent();
             _homeViewModel = new HomeViewModel();
-            _homeViewModel.GetUser();  
-            GetSetUserData(); 
+            Loaded += HomeView_Loaded; 
+        }
+        private void HomeView_Loaded(object sender, RoutedEventArgs e)
+        {
+            _homeViewModel.GetUser(); 
+            GetSetUserData();
         }
 
         public void GetSetUserData()
@@ -39,9 +43,16 @@ namespace Expensesmanager.MVMM.View
             string firstName = _homeViewModel.FirstName;
             string lastName = _homeViewModel.LastName;
             double monthlyIncome = _homeViewModel.MonthlyIncome;
+            int RemainingDays = 9;
 
             Greet_TextBlock.Text = $"Hallo {firstName} {lastName}";
             IncomeTextBlock.Text = monthlyIncome.ToString();
+
+            // Orange warning RemainingDaysTextBlock
+            if (RemainingDays < 15) RemainingDaysTextBlock.Foreground = (Brush)new BrushConverter().ConvertFromString("#FFBD2E");
+            // Red warning RemainingDaysTextBlock
+            if (RemainingDays <= 10) RemainingDaysTextBlock.Foreground = (Brush)new BrushConverter().ConvertFromString("#FF5F56");
+        }
     }
-  }
 }
+
