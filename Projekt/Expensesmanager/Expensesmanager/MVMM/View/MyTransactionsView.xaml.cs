5ff5870a2f09fa477666de8fde68fe2a449f63e0
Luis Allamand <Luis@g-12.ch>
2025-03-08 01:24:28 +0100

    Changed somethings to fix it

--- Projekt/Expensesmanager/Expensesmanager/MVMM/View/MyTransactionsView.xaml.cs
+++ Projekt/Expensesmanager/Expensesmanager/MVMM/View/MyTransactionsView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Expensesmanager.MVMM.ViewModel;
+using Expensesmanager.ViewModel;
+using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +23,25 @@ namespace Expensesmanager.MVMM.View
     /// </summary>
     public partial class MyTransactionsView: UserControl
     {
-        private MyTransactionsViewModel _MyTransactions;
         public MyTransactionsView()
         {
             InitializeComponent();
+            //Records.Add(new Record { Betrag = 243, Datum = new DateTime(2025, 1, 1), Kategorie = "Kategorie1" });
+            //Records.Add(new Record { Betrag = 23, Datum = new DateTime(2025, 1, 1), Kategorie = "Kategorie1" });
+            //Records.Add(new Record { Betrag = 23, Datum = new DateTime(2025, 1, 3), Kategorie = "Kategorie3" });
+            //Records.Add(new Record { Betrag = 65, Datum = new DateTime(2025, 1, 4), Kategorie = "Kategorie2" });
+            //Records.Add(new Record { Betrag = 123, Datum = new DateTime(2025, 1, 6), Kategorie = "Kategorie4" });
+
+            //// Setzen Sie den DataContext des Fensters auf sich selbst
+            //DataContext = this;
         }
+        // public ObservableCollection<Record> Records { get; set; } = new ObservableCollection<Record>();
     }
+    //public class Record
+    //{
+    //    public double Betrag { get; set; }
+    //    public DateTime Datum { get; set; }
+    //    public string Kategorie { get; set; }
+    //}
 }
+    
--- Projekt/Expensesmanager/Expensesmanager/MVMM/ViewModel/MyTransactionsViewModel.cs
+++ Projekt/Expensesmanager/Expensesmanager/MVMM/ViewModel/MyTransactionsViewModel.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using SQLitePCL;
+using System;
+using System.IO;
+using System.Windows;
+using System.Security.Cryptography;
+using Expensesmanager.Core;
+using Expensesmanager.ViewModel;
+using System.Windows.Documents;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Expensesmanager.MVMM.ViewModel
 {
