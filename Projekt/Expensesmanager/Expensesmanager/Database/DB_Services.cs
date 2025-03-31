85d5a53cf001f9031b07c71160577a9785f059c1
Janik Stucki <Janik_Stucki@sluz.ch>
2025-03-31 10:16:58 +0200

    Added Skeleton of the single connection for the database.

---
+++ Projekt/Expensesmanager/Expensesmanager/Database/DB_Services.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expensesmanager.Database
+{
+  public class DB_Services
+  {
+    private string connection_string = App.ConnectionString;
+
+
+    public DataTable ExecuteQuery(string query, Dictionary<string, object> parameters = null)
+    {
+      DataTable resultTable = new DataTable();
+
+      try
+      {
+
+      } catch (Exception ex) 
+      {
+      
+      }
+
+      return resultTable;
+    }
+  }
+}
