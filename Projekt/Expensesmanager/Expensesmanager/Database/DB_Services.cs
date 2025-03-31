2dc0dd3dd14bea55f99d67fca3cf91565bce359b
Janik Stucki <Janik_Stucki@sluz.ch>
2025-03-31 10:27:35 +0200

    Added the Code for the ExecuteQuery function

--- Projekt/Expensesmanager/Expensesmanager/Database/DB_Services.cs
+++ Projekt/Expensesmanager/Expensesmanager/Database/DB_Services.cs
@@ -5,12 +5,13 @@ using System.Linq;
 using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
 
 namespace Expensesmanager.Database
 {
   public class DB_Services
   {
-    private string connection_string = App.ConnectionString;
+    private string connectionString = App.ConnectionString;
 
 
     public DataTable ExecuteQuery(string query, Dictionary<string, object> parameters = null)
@@ -19,10 +20,29 @@ namespace Expensesmanager.Database
 
       try
       {
+        using (var connection = new SqliteConnection(connectionString))
+        {
+          using (var command = new SqliteCommand(query, connection))
+          {
+            if(parameters != null)
+            {
+              foreach (var param in parameters) 
+              {
+                command.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
+              }
+            }
 
-      } catch (Exception ex) 
+              connection.Open();
+
+              using (SqliteDataReader reader = command.ExecuteReader()) 
+              {
+                resultTable.Load(reader);
+              }
+            }
+          }
+        } catch (Exception ex) 
       {
-      
+        Console.WriteLine(ex.ToString());
       }
 
       return resultTable;
